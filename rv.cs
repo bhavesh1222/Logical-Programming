48e038cdcbfe2a5c6f308b87bed5fc06105ff656
Bhavesh Bhadane Patil <113571629+bhavesh1222@users.noreply.github.com>
2022-10-18 09:52:05 +0530

    reverse number

---
+++ rv.cs
@@ -0,0 +1,24 @@
+﻿namespace LogicalProgramming
+{
+    public static class ReverseNumber
+    {
+        public static void Main(string[]args)
+        {
+            int n, reverse = 0, rem;
+            Console.WriteLine("Enter a Number");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            while (n != 0) ;
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n = n / 10;
+
+
+            }
+            Console.WriteLine("Reverse number is" + reverse);
+        }
+
+
+    }
+}
